466616667097c78a63c855ab4f5b9f1beb614e1b
Farid Azhari <faridazhari07@gmail.com>
2025-05-07 05:36:50 +0700

    modular structure

--- src/app.js
+++ src/app.js
@@ -8,9 +8,3 @@ app.use(express.json());
 
 app.use("/users", userRoutes);
 app.use("/request", requestRoutes);
-
-const PORT = process.env.PORT || 3100;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
