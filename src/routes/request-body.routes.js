3f5d5ce673a4479ec71ab2f2d97fd3ec547c829f
Farid Azhari <faridazhari07@gmail.com>
2025-05-08 13:44:25 +0700

    add .js

--- src/routes/request-body.routes.js
+++ src/routes/request-body.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getRequestBody } from "../controller/request-body.controller";
+import { getRequestBody } from "../controller/request-body.controller.js";
 
 const router = Router();
 router.get("/", getRequestBody);
